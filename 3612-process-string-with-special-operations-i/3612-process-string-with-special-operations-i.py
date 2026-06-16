8a1116eaa6709a669c4d0c9a469d73e02d1518c8
Meka Viraj <mekaviraj1749@gmail.com>
2026-06-16 12:22:23 +0530

    Time: 11 ms (70.39%), Space: 22.8 MB (100%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = ""
+        for i in s :
+            if i == "#" :
+                res += res
+            elif i == "*" :
+                if res :
+                    res = res[0:len(res) -1]
+            elif  i == "%" :
+                res = res[::-1]
+            else :
+                res += i
+            print(res)
+        return res
